c29e36aa1462f9a61f03540727e4af009cc0b0fa
Samiksha <shende.samiksha@rediffmail.com>
2021-04-20 12:13:15 +0530

    [Samiksha].Add code to check the attendance of employee

---
+++ empWage.js
@@ -0,0 +1,11 @@
+//UC 1 : Check Employee Present
+{
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if (empCheck == IS_ABSENT) {
+        console.log("UC1 - Employee is ABSENT");
+    }
+    else {
+        console.log("UC1 - Employee is PRESENT");
+    }
+}
